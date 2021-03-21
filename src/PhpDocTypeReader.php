8e9d711cc612ec10af8a32251d21b8b8a0f23d50
sji <sji@sj-i.dev>
2021-03-21 15:47:45 +0900

    add a implementation for getVarTypes() to pass the test

---
+++ src/PhpDocTypeReader.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the sj-i/phpdoc-type-reader package.
+ *
+ * (c) sji <sji@sj-i.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace PhpDocTypeReader;
+
+use PhpDocTypeReader\Type\IntType;
+use PhpDocTypeReader\Type\Type;
+
+final class PhpDocTypeReader
+{
+    public function getVarTypes(string $doc_comment): Type
+    {
+        return new IntType();
+    }
+}
